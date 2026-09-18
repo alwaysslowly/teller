e32639160f3e53835946aedbad939cd030fa9097
MoonBeom <tjansqja456@naver.com>
2026-09-18 10:27:55 +0900

    직원 비밀번호 해시 변환 완료

--- src/main/java/com/study/teller/TellerApplication.java
+++ src/main/java/com/study/teller/TellerApplication.java
@@ -1,13 +1,30 @@
 package com.study.teller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.study.teller.common.PasswordUtil;
+import com.study.teller.mapper.EmpMapper;
 
 @SpringBootApplication
 public class TellerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TellerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TellerApplication.class, args);
+    }
 
+    /** 시작 시 직원 비밀번호를 해시로 변환 (학습용) */
+    @Bean
+    ApplicationRunner initPassword(EmpMapper empMapper) {
+        return args -> {
+            String[] empNos = { "E12345", "E99999", "E54321" };
+            for (String empNo : empNos) {
+                empMapper.updatePasswd(empNo, PasswordUtil.encode("1234"));
+            }
+            System.out.println(">> 직원 비밀번호 해시 변환 완료");
+        };
+    }
 }
